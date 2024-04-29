985f7583182dcbbf753e35ab04d4bdef717c8451
Anas Yousef <44998563+anas-yousef@users.noreply.github.com>
2024-04-29 21:29:49 +0300

    Handled no internet connection, and checking user log in on start

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -1,20 +1,56 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:touch_cut_app/providers/internet_connection_provider.dart';
 
 typedef TokenProvider = Future<String?> Function();
+
+class AccessTokenNotFoundException implements Exception {
+  const AccessTokenNotFoundException({
+    this.errorMessage,
+  });
+  final String? errorMessage;
+  @override
+  String toString() {
+    return errorMessage ?? 'Access token was not found in local storage';
+  }
+}
+
+class AccessTokenCantRefresh implements Exception {
+  const AccessTokenCantRefresh({
+    this.errorMessage,
+  });
+  final String? errorMessage;
+  @override
+  String toString() {
+    return errorMessage ?? 'Access token was not refreshed';
+  }
+}
+
+class RefreshTokenNotFoundException implements Exception {
+  const RefreshTokenNotFoundException({
+    this.errorMessage,
+  });
+  final String? errorMessage;
+  @override
+  String toString() {
+    return errorMessage ?? 'Refresh token was not found in local storage';
+  }
+}
 
 class ApiClient {
   final String baseUrl;
   final FlutterSecureStorage secureStorage;
   final http.Client httpClient;
-
-  const ApiClient(
-      {required this.baseUrl,
-      required this.secureStorage,
-      required this.httpClient});
+  final Connectivity connectivity;
+  ApiClient({
+    required this.baseUrl,
+    required this.secureStorage,
+    required this.httpClient,
+  }) : connectivity = Connectivity();
 
   Future<String?> _getAccessToken() async {
     return await secureStorage.read(key: 'access_token');
@@ -24,7 +60,7 @@ class ApiClient {
     await secureStorage.write(key: 'access_token', value: accessToken);
   }
 
-  Future<void> _clearAccessToken() async {
+  Future<void> clearAccessToken() async {
     await secureStorage.delete(key: 'access_token');
   }
 
@@ -36,19 +72,23 @@ class ApiClient {
     await secureStorage.write(key: 'refresh_token', value: refreshToken);
   }
 
-  Future<void> _clearRefreshToken() async {
+  Future<void> clearRefreshToken() async {
     await secureStorage.delete(key: 'refresh_token');
   }
 
   Future<void> _refreshAccessToken() async {
+    print('Refershing access token');
     final refreshToken = await _getRefreshToken();
     if (refreshToken != null) {
       try {
         final response = await httpClient.post(
-          Uri.parse('$baseUrl/auth/refresh'),
-          body: {'refresh_token': refreshToken},
+          Uri.parse('$baseUrl/api/auth/refresh'),
+          body: jsonEncode({'refresh_token': refreshToken}),
+          headers: {
+            HttpHeaders.contentTypeHeader: ContentType.json.value,
+          },
         );
-        final responseData = jsonDecode(response.body) as Map<String, String>;
+        final responseData = jsonDecode(response.body) as Map<String, dynamic>;
         if (response.statusCode == HttpStatus.ok) {
           // TODO Unite futures
           final newAccessToken = responseData['access_token'] as String;
@@ -57,16 +97,18 @@ class ApiClient {
           await _saveRefreshToken(refreshToken);
         } else {
           // Handle error response
-          throw Exception(
-              'Failed to refresh access token. ${response.statusCode}, error: ${responseData['error_message']}');
+          throw AccessTokenCantRefresh(
+              errorMessage:
+                  'Failed to refresh access token. ${response.statusCode}, error: ${responseData['error_message']}');
         }
       } catch (e) {
+        print('Failed to refresh access token: $e');
         // Handle network or server errors
-        throw Exception('Failed to refresh access token: $e');
+        rethrow;
       }
     } else {
       // Handle case where refresh token is not available
-      throw Exception('No refresh token available');
+      throw const RefreshTokenNotFoundException();
     }
   }
 
@@ -88,10 +130,10 @@ class ApiClient {
       // }
       return response;
     } on TimeoutException {
-      throw Exception('Request timeout. Please try again');
+      throw TimeoutException('Request timeout. Please try again');
     } catch (e) {
       // Handle network or server errors
-      throw Exception('Failed to make request: $e');
+      rethrow;
     }
   }
 
@@ -103,9 +145,10 @@ class ApiClient {
   }) async {
     final accessToken = await _getAccessToken();
     if (accessToken == null) {
-      throw Exception('No access token available');
+      throw const AccessTokenNotFoundException();
     }
 
+    print('Doing request to $baseUrl$path');
     final request = http.Request(method, Uri.parse('$baseUrl$path'))
       ..headers['Authorization'] = 'Bearer $accessToken'
       ..headers['Content-Type'] = 'application/json'
@@ -154,4 +197,84 @@ class ApiClient {
       return await _authenticatedRequest('PUT', path, body: body);
     });
   }
+
+  Future<bool?> userIsLoggedIn({
+    required InternetConnectionProvider internetConnectionProvider,
+  }) async {
+    try {
+      final response = await makeRequest(
+        method: 'GET',
+        path: '/api/v1/validateToken',
+        internetConnectionProvider: internetConnectionProvider,
+      );
+      if (response == null) {
+        return null;
+      }
+      if (response.statusCode == HttpStatus.ok) {
+        return true;
+      }
+    } on TimeoutException {
+      throw TimeoutException('Request timeout. Please try again');
+    } catch (e) {
+      print('Got exception when validating user session, $e');
+      if (e is AccessTokenCantRefresh ||
+          e is AccessTokenNotFoundException ||
+          e is RefreshTokenNotFoundException) {
+        return false;
+      }
+      rethrow;
+    }
+    return false;
+  }
+
+  Future<http.Response?> makeRequest({
+    required InternetConnectionProvider internetConnectionProvider,
+    required String method, // HTTP method type: GET, DELETE, PATCH, PUT, POST
+    required String path,
+    Map<String, dynamic>? body,
+  }) async {
+    bool isConnected =
+        await internetConnectionProvider.checkInternetConnectivity();
+    if (!isConnected) {
+      // Return null as the response since there's no internet connection
+      return null;
+    }
+
+    try {
+      final http.Response response;
+      switch (method) {
+        case 'GET':
+          response = await get(path);
+          break;
+        case 'DELETE':
+          response = await delete(path);
+          break;
+        case 'PATCH':
+          response = await patch(path, body: body!);
+          break;
+        case 'PUT':
+          response = await put(path, body: body!);
+          break;
+        case 'POST':
+          response = await post(path, body: body!);
+          break;
+        default:
+          throw ArgumentError('Invalid HTTP method: $method');
+      }
+
+      // Handle response here
+      return response;
+    } catch (e) {
+      // Handle other exceptions
+      print('Error when making request: $e');
+      rethrow;
+    }
+  }
+
+// Future<Map<String, dynamic>> get(String chatRoomId) async {
+//     final uri = Uri.parse('$_baseUrl/chat-rooms/id/$chatRoomId/messages');
+//     final response = await _handleRequest(
+//         (headers) => _httpClient.get(uri, headers: headers));
+//     return response;
+//   }
 }
